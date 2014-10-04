4c0ff2e0b22b97cd2a3eee8e9530d2e9c1312309
Joshua Dong <jdong42@gmail.com>
2014-10-04 18:08:57 -0500

    How to an pthread?

--- src/sample_bot.c
+++ src/sample_bot.c
@@ -5,15 +5,22 @@
 #include "bot.h"
 #include "client.h"
 
-void sample_main(void* dummy) {
-    struct timespec req = {0, 50000000};
-    struct timespec rem;
-    while(1) {
-        nanosleep(&req, &rem);
-    }
+void sample_main(void* bot) {
+//    bot_t *an_bot = (bot_t*)bot;
+//    struct timespec req = {1, 50000000};
+//    struct timespec rem;
+//
+//    join_server(bot, "25567", "10.10.2.16", "25565");
+//    send_handshaking_serverbound_handshake(an_bot, 47, "localhost", 25565, 2);
+//    send_login_serverbound_login(an_bot, an_bot->name);
+//
+//    while(1) {
+//        nanosleep(&req, &rem);
+//    }
 }
 
 int main() {
-    /* bot_t *batman_bot = init_bot("batman", *sample_main); */
-    /* client_run(batman_bot, 1); */
+//    bot_t *batman_bot = init_bot("batman", *sample_main);
+//    client_run(batman_bot, 1);
+    return 0;
 }
