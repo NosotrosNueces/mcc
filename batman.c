56a4b76c80e79d5c546caabd66e18d7c67d51b30
Joshua <jdong42@gmail.com>
2017-02-22 12:55:10 -0600

    try new plants

--- batman.c
+++ batman.c
@@ -7,10 +7,13 @@
 #include "protocol.h"
 #include "capture.h"
 #include "break.h"
-#include "throttle.h"
 
 #define DEFAULT_SERVER_NAME "localhost"
 #define DEFAULT_SERVER_PORT "25565"
+
+void *_read(void *buffer, void *storage, size_t size);
+void *_read_vint32(void *buffer, int32_t *val);
+void *_read_string(void *buffer, char **strptr, int32_t *len);
 
 struct player {
     char uuid[16];
@@ -72,11 +75,11 @@ void spawn_player(
             uint8_t pitch,
             struct entity_metadata *metadata
             ) {
-    struct player *p = list_find(&player_list, compare_uuid, uuid);
-    char *message = malloc(strlen(p->name) + strlen("Hello !") + 1);
-    snprintf(message, strlen(p->name) + strlen("Hello !") + 1, "Hello %s!", p->name);
-    send_play_serverbound_chat_message(bot, message);
-    free(message);
+    //struct player *p = list_find(&player_list, compare_uuid, uuid);
+    //char *message = malloc(strlen(p->name) + strlen("Bonjour, .") + 1);
+    //snprintf(message, strlen(p->name) + strlen("Bonjour, .") + 1, "Bonjour, %s.", p->name);
+    //send_play_serverbound_chat_message(bot, message);
+    //free(message);
 }
 
 void player_list_item (
@@ -89,10 +92,10 @@ void player_list_item (
         switch(action_type) {
             case PLAYER_LIST_ADD_PLAYER:
                 printf("Hello %s!\n", player_actions[i].add_player.name);
-                char *message = malloc(strlen(player_actions[i].add_player.name) + strlen("Hello !") + 1);
-                snprintf(message, strlen(player_actions[i].add_player.name) + strlen("Hello !") + 1, "Hello %s!", player_actions[i].add_player.name);
-                send_play_serverbound_chat_message(bot, message);
-                free(message);
+                //char *message = malloc(strlen(player_actions[i].add_player.name) + strlen("Hello !") + 1);
+                //snprintf(message, strlen(player_actions[i].add_player.name) + strlen("Hello !") + 1, "Hello %s!", player_actions[i].add_player.name);
+                //send_play_serverbound_chat_message(bot, message);
+                //free(message);
                 struct player *new_player = malloc(sizeof(struct player));
                 memcpy(new_player->uuid, player_actions[i].uuid, sizeof(new_player->uuid));
                 new_player->name = malloc(strlen(player_actions[i].add_player.name) + 1);
@@ -174,100 +177,6 @@ void update_health (
 
 double player_x, player_y, player_z;
 
-static inline double distance(struct mc_position p1, struct mc_position p2) {
-    double dx = p1.x - p2.x;
-    double dy = p1.y - p2.y;
-    double dz = p1.z - p2.z;
-    return sqrt(dx * dx + dy * dy + dz * dz);
-}
-
-#define POSITION_X(p)   ((int64_t)(p) >> 38)
-#define POSITION_Y(p)   (((int64_t)(p) >> 26) & 0xFF)
-#define POSITION_Z(p)   ((int64_t)(p) << 38 >> 38)
-
-void break_stop_cb (uv_timer_t* handle) {
-     /* start break timer */
-    struct bot_agent *bot = handle->data;
-    send_play_serverbound_player_digging(
-        bot,
-        2,
-        bot->block_break_location,
-        0
-        );
-    uv_timer_stop(handle);
-    bot->is_breaking = 0;
-}
-
-void start_block_break(struct bot_agent *bot, int32_t block_id, struct mc_position location) {
-    /* start break timer */
-    if (bot->is_breaking) {
-        return;
-    }
-    double break_time = block_break_time_hand(block_id);
-    if (break_time == INFINITY) {
-        return;
-    }
-    uint64_t break_time_milli = (uint64_t)(break_time * 1000);
-
-    bot->block_break_location = location;
-    
-    send_play_serverbound_player_digging(
-        bot,
-        0,
-        location,
-        1
-        );
-
-    uv_timer_init(&bot->loop, &bot->block_break_timer);
-    uv_timer_start(&bot->block_break_timer, break_stop_cb, break_time_milli, 0);
-    bot->is_breaking = 1;
-}
-
-void stop_block_break(struct bot_agent *bot) {
-    uv_timer_stop(&bot->block_break_timer);
-    bot->is_breaking = 0;
-}
-
-void block_change_cb(
-            struct bot_agent *bot,
-            struct mc_position location,
-            vint32_t block_id
-            ) {
-    if (distance(location, bot->position) < 10) {
-        const char *block_name_str = block_name(block_id);
-        char message[512];
-        snprintf(message,
-                sizeof(message),
-                "%s: (%d, %d, %d)\n",
-                block_name_str,
-                (int)location.x,
-                (int)location.y,
-                (int)location.z
-                );
-        printf("%s", message);
-        send_play_serverbound_chat_message(bot, message);
-
-        snprintf(message,
-                sizeof(message),
-                "    Hardness: %f\n",
-                block_hardness(block_id)
-                );
-        printf("%s", message);
-        send_play_serverbound_chat_message(bot, message);
-
-        snprintf(message,
-                sizeof(message),
-                "    Break time by hand: %f\n",
-                block_break_time_hand(block_id)
-                );
-        printf("%s", message);
-        send_play_serverbound_chat_message(bot, message);
-        printf("    metadata: 0x%02x\n", block_id & 15);
-        
-        start_block_break(bot, block_id, location);
-    }
-}
-
 void player_position_and_look_cb (
             struct bot_agent *bot,
             double x,
@@ -281,7 +190,13 @@ void player_position_and_look_cb (
     player_x = x;
     player_y = y;
     player_z = z;
+
+    printf("Location update: %.0f %.0f %.0f\n", x, y, z);
 }
+
+#define POSITION_X(p)   ((int64_t)(p) >> 38)
+#define POSITION_Y(p)   (((int64_t)(p) >> 26) & 0xFF)
+#define POSITION_Z(p)   ((int64_t)(p) << 38 >> 38)
 
 void spawn_mob_cb (
     struct bot_agent *bot,
@@ -342,19 +257,6 @@ void spawn_mob_cb (
     }*/
 }
 
-void clientbound_play_entity_look_and_relative_move_cb(
-    struct bot_agent *bot,
-    vint32_t entity_id,
-    int16_t delta_x,
-    int16_t delta_y,
-    int16_t delta_z,
-    uint8_t yaw, 
-    uint8_t pitch,
-    bool on_ground
-    ) {
-    printf("entity move: %d %d %d\n", delta_x, delta_y, delta_z);
-}
-
 
 int main(int argc, char *argv[], char **envp)
 {
@@ -378,25 +280,21 @@ int main(int argc, char *argv[], char **envp)
             return -1;
     }
 
-    init_bot(&bot, "Sintralin");
+    init_bot(&bot, "batman");
     bot.callbacks.clientbound_play_player_list_item_cb = player_list_item;
-    //bot.callbacks.clientbound_play_chunk_data_cb = chunk_data;
     bot.callbacks.clientbound_play_spawn_player_cb = spawn_player;
     bot.callbacks.clientbound_play_update_health_cb = update_health;    
-    bot.callbacks.clientbound_play_block_change_cb = block_change_cb;
     bot.callbacks.clientbound_play_player_position_and_look_cb = player_position_and_look_cb;
 
     bot.callbacks.clientbound_play_spawn_mob_cb = spawn_mob_cb;
-    bot.callbacks.clientbound_play_entity_look_and_relative_move_cb = clientbound_play_entity_look_and_relative_move_cb;
 
     list_init(&player_list);
 
     /* packet capture */
     bot.capture_enabled = 1;
-    init_capture(&bot, capture_file);
+    init_capture(&bot, "capture");
 
     join_server_hostname(&bot, server_name, server_port);
-    init_throttle(&bot, 1000);
     uv_run(&bot.loop, UV_RUN_DEFAULT);
     while(1);
     uv_loop_close(&bot.loop);
